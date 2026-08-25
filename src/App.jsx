8350e6e59c3f0155e4fccfb682ba10d922bb372e
Harsha <repalaharsha@gmail.com>
2026-08-25 13:04:49 +0530

    Implement GitHub Pages deployment workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { BookingProvider } from './context/BookingContext'
 import AppRoutes from './routes/AppRoutes'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <BookingProvider>
           <AppRoutes />
         </BookingProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
